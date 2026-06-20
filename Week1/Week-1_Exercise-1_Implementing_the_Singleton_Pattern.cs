0dc652cea469ae6b17cff4fd79e7f4014045fe04
Tyson_Spandan <144247422+Spandan3251@users.noreply.github.com>
2026-06-21 02:14:29 +0530

    Add files via upload
    Week 1 hands on committed successfully

---
+++ Week1/Week-1_Exercise-1_Implementing_the_Singleton_Pattern.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace DesignPatternsPrinciples
+{
+    public class Logger
+    {
+        private static readonly Logger instance = new Logger();
+
+        private Logger()
+        {
+            Console.WriteLine("Logger has been initialized.");
+        }
+
+        public static Logger Instance => instance;
+
+        public void LogMessage(string message)
+        {
+            Console.WriteLine($"[Log] {message}");
+        }
+    }
+
+
+    class LoggerMain
+    {
+        static void Main(string[] args)
+        {
+            Logger logger1 = Logger.Instance;
+            Logger logger2 = Logger.Instance;
+
+            logger1.LogMessage("System is starting.");
+            logger2.LogMessage("Executing main workflow...");
+
+            if (ReferenceEquals(logger1, logger2))
+            {
+                Console.WriteLine("Confirmed: logger1 and logger2 refer to the same instance. So SINGLETON is WORKING");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed: Different logger instances created.");
+            }
+        }
+    }
+}
